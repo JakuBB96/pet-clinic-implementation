83d1fbb9cc7f00e76e907bd1c2a41dfb60f89ed6
JakuBB96 <JakuBB96@javadev.com>
2020-04-09 19:52:23 +0200

    Implemented Vet Service, closes #41

---
+++ pet-clinic-data/src/main/java/com/barancewicz/petclinic/services/springdatajpa/VetServiceImpl.java
@@ -0,0 +1,50 @@
+package com.barancewicz.petclinic.services.springdatajpa;
+
+
+import com.barancewicz.petclinic.model.Vet;
+import com.barancewicz.petclinic.repositories.VetRepository;
+import com.barancewicz.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+@Service
+@Profile("springdatajpa")
+public class VetServiceImpl implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetServiceImpl(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        Optional<Vet> vetOptional = vetRepository.findById(aLong);
+        return vetOptional.orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
