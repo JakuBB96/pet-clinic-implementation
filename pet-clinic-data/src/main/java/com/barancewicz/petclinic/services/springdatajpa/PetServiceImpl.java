e0151afd883e7658872a07f8fe5c883916970567
JakuBB96 <JakuBB96@javadev.com>
2020-04-09 19:52:55 +0200

    Implemented Pet n PetType Service, closes #40

---
+++ pet-clinic-data/src/main/java/com/barancewicz/petclinic/services/springdatajpa/PetServiceImpl.java
@@ -0,0 +1,49 @@
+package com.barancewicz.petclinic.services.springdatajpa;
+
+import com.barancewicz.petclinic.model.Pet;
+import com.barancewicz.petclinic.repositories.PetRepository;
+import com.barancewicz.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+@Service
+@Profile("springdatajpa")
+public class PetServiceImpl implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetServiceImpl(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        Optional<Pet> petOptional = petRepository.findById(aLong);
+        return petOptional.orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/barancewicz/petclinic/services/springdatajpa/PetTypeServiceImpl.java
@@ -0,0 +1,50 @@
+package com.barancewicz.petclinic.services.springdatajpa;
+
+
+import com.barancewicz.petclinic.model.PetType;
+import com.barancewicz.petclinic.repositories.PetTypeRepository;
+import com.barancewicz.petclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+@Service
+@Profile("springdatajpa")
+public class PetTypeServiceImpl implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeServiceImpl(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        Optional<PetType> petTypeOptional = petTypeRepository.findById(aLong);
+        return petTypeOptional.orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> types = new HashSet<>();
+        petTypeRepository.findAll().forEach(types::add);
+        return types;
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
