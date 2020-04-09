27f004739b306457aaf7d3a6c58c01d71596020a
JakuBB96 <JakuBB96@javadev.com>
2020-04-09 19:18:16 +0200

    Converted Owner POJO to JPA entity, closes #34

--- pet-clinic-data/src/main/java/com/barancewicz/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/barancewicz/petclinic/model/Owner.java
@@ -1,13 +1,21 @@
 package com.barancewicz.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
-
+    @Column(name = "address")
     private String address;
+    @Column(name = "city")
     private String city;
+    @Column(name = "telephone")
     private String telephone;
+
+    //if i delete owner, all pets are going to be deleted
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
 
