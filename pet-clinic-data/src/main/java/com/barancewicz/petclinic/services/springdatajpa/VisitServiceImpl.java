6915eb4ad06a8490288a865e313d14b87cb82e1b
JakuBB96 <JakuBB96@javadev.com>
2020-04-09 20:09:21 +0200

    Implemented Visits both Map and JPA services , closes #43, closes #54

---
+++ pet-clinic-data/src/main/java/com/barancewicz/petclinic/services/springdatajpa/VisitServiceImpl.java
@@ -0,0 +1,47 @@
+package com.barancewicz.petclinic.services.springdatajpa;
+
+import com.barancewicz.petclinic.model.Visit;
+import com.barancewicz.petclinic.repositories.VisitRepository;
+import com.barancewicz.petclinic.services.VisitService;
+import org.graalvm.compiler.nodes.calc.IntegerDivRemNode;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class VisitServiceImpl implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitServiceImpl(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        Optional<Visit> visitOptional = visitRepository.findById(aLong);
+        return visitOptional.orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+       return visitRepository.save(object);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+       Set<Visit> visits = new HashSet<>();
+       visitRepository.findAll().forEach(visits::add);
+       return visits;
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
